30876243301bf3da87f0d99a5dad8ce628566bb0
Louis Brent <louisbrent1992@gmail.com>
2021-10-07 10:15:37 -0700

    Home page finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+          <Navbar />
+          <Switch>
+              <Route path='/' exact component={Home}/>
+          </Switch>
+      </Router>
+    </>
   );
 }
 
